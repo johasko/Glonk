fff0d8dce970eeec720adabb5a3a046789a2517c
Johannes Skøien <johannesskoien@1x-193-157-185-189.uio.no>
2018-10-09 17:29:29 +0200

    first shit

---
+++ index.php
@@ -0,0 +1,19 @@
+<! DOCTYPE html>
+<html lang="no">
+    <head>
+            <meta charset="utf-8" />
+            <title>Glonk</title>
+            <link rel="shortcut icon" href="Glonk2.png" type="image/x-icon">
+            <link rel="stylesheet" type="text/css" href="stil.css" />
+    </head>
+    <body>
+        <div class="Glonkvert">
+             <div class="Glonkhor">   
+                <img
+                src="Glonk2.png"
+                alt="Glonk"
+                width="614"/>
+            </div> 
+        </div>
+    </body>
+</html> 
